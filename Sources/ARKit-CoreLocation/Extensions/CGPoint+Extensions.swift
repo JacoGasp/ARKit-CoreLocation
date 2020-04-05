3a967c593a922e04aa58cfdbd5c0c10059677cfb
Jacopo Gasparetto <jacopo.gasparetto@gmail.com>
2020-04-05 11:49:55 +0200

    Removed unused distance function in favour of quaternions

--- Sources/ARKit-CoreLocation/Extensions/CGPoint+Extensions.swift
+++ Sources/ARKit-CoreLocation/Extensions/CGPoint+Extensions.swift
@@ -21,8 +21,4 @@ extension CGPoint {
 
         return x + y <= radiusSquared
     }
-    
-    func distance(to point: CGPoint) -> CGFloat {
-            return sqrt(pow(x - point.x, 2) + pow(y - point.y, 2))
-    }
 }
